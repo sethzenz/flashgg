e60b27e87cae1b3a849b691d3cbb7d85908e7163
Seth <Seth.Zenz@cern.ch>
2015-05-27 17:37:13 +0100

    copy files from globe, include script to input MC scale and uncertainties

--- Systematics/python/flashggPhotonSmear_cfi.py
+++ Systematics/python/flashggPhotonSmear_cfi.py
@@ -1,23 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
-scaleBins = cms.VPSet(cms.PSet( Range = cms.string("pt>10.&&pt<15."), Shift = cms.double(0.003), Uncertainty = cms.double(0.001) ),
-                      cms.PSet( Range = cms.string("pt>15.&&pt<20."), Shift = cms.double(-0.001), Uncertainty = cms.double(0.0005) ),
-                      cms.PSet( Range = cms.string("pt>20."), Shift = cms.double(0.002), Uncertainty = cms.double(0.0004) )
+scaleBins = cms.VPSet(
+          cms.PSet( Range = cms.string("abs(eta)>0.000&&abs(eta)<1.000&&r9>0.94&&r9<999.00"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00050) )
+          cms.PSet( Range = cms.string("abs(eta)>0.000&&abs(eta)<1.000&&r9>-999.00&&r9<0.94"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00050) )
+          cms.PSet( Range = cms.string("abs(eta)>1.000&&abs(eta)<1.500&&r9>0.94&&r9<999.00"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00060) )
+          cms.PSet( Range = cms.string("abs(eta)>1.000&&abs(eta)<1.500&&r9>-999.00&&r9<0.94"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00120) )
+          cms.PSet( Range = cms.string("abs(eta)>1.500&&abs(eta)<2.000&&r9>-999.00&&r9<0.94"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00200) )
+          cms.PSet( Range = cms.string("abs(eta)>1.500&&abs(eta)<2.000&&r9>0.94&&r9<999.00"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00300) )
+          cms.PSet( Range = cms.string("abs(eta)>2.000&&abs(eta)<3.000&&r9>-999.00&&r9<0.94"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00200) )
+          cms.PSet( Range = cms.string("abs(eta)>2.000&&abs(eta)<3.000&&r9>0.94&&r9<999.00"), Shift = cms.double(0.00000), Uncertainty = cms.double(0.00300) )
                       )
+
 
 flashggSmearPhoton = cms.EDProducer('FlashggDiPhotonSystematicProducer',
 		src = cms.untracked.InputTag('flashggDiPhotons'),
 		SystMethods = cms.VPSet(
-        				# cms.PSet( PhotonMethodName = cms.string("FlashggSimplePhotonSmear"),
-                                        #          MethodName = cms.string("FlashggDiPhotonFromPhoton"),
-                                        #          Label = cms.string("exampleSystAlpha"),
-                                        #          Sigma = cms.double(3.0),
-                                        #          NSigmas = cms.vint32(-1,1)),
-                                        # cms.PSet( PhotonMethodName = cms.string("FlashggSimplePhotonSmear"),
-                                        #          MethodName = cms.string("FlashggDiPhotonFromPhoton"),
-                                        #          Label = cms.string("exampleSystBravo"),
-                                        #          Sigma = cms.double(1.0),
-                                        #          NSigmas = cms.vint32(-2,-1,4,7)),
                                         cms.PSet( PhotonMethodName = cms.string("FlashggPhotonScaleString"),
                                                   MethodName = cms.string("FlashggDiPhotonFromPhoton"),
                                                   Label = cms.string("exampleSystCharlieBarrel"),
