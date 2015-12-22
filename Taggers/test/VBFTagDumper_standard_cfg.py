7f284afe68d6a060871aeeeb5809d7d9a8a47365
Seth Zenz <seth.zenz@cern.ch>
2015-12-22 11:17:46 +0100

    option to dump jet systematics

--- Taggers/test/VBFTagDumper_standard_cfg.py
+++ Taggers/test/VBFTagDumper_standard_cfg.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env cmsRun
 
+doSystematics = True
+requireTriggerOnData = True
+
 import FWCore.ParameterSet.Config as cms
 import FWCore.Utilities.FileUtils as FileUtils
 from FWCore.ParameterSet.VarParsing import VarParsing
 from flashgg.MetaData.samples_utils import SamplesManager
+from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag,cloneProcessingSnippet
+from flashgg.MetaData.JobConfig import customize
 
 process = cms.Process("VBFTagsDumper")
+
+if doSystematics:
+    process.load("Configuration.StandardSequences.GeometryDB_cff")
+    process.load("Configuration.StandardSequences.MagneticField_cff")
+    process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+    from Configuration.AlCa.GlobalTag import GlobalTag
+    process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v4' # keep updated for JEC
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
@@ -22,8 +34,24 @@ from flashgg.Taggers.flashggTagOutputCommands_cff import tagDefaultOutputCommand
 import flashgg.Taggers.dumperConfigTools as cfgTools
 from  flashgg.Taggers.tagsDumpers_cfi import createTagDumper
 
+if doSystematics:
+    from flashgg.Taggers.flashggTags_cff import UnpackedJetCollectionVInputTag
+    from flashgg.Systematics.flashggJetSystematics_cfi import createJetSystematics
+    process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService")
+    jetSystematicsInputTags = createJetSystematics(process,UnpackedJetCollectionVInputTag)
+
 process.load("flashgg.Taggers.flashggTagSequence_cfi")
 process.load("flashgg.Taggers.flashggTagTester_cfi")
+
+if doSystematics:
+    process.flashggTagSequence.remove(process.flashggUnpackedJets)
+    for i in range(len(UnpackedJetCollectionVInputTag)):
+        massSearchReplaceAnyInputTag(process.flashggTagSequence,UnpackedJetCollectionVInputTag[i],jetSystematicsInputTags[i])
+    process.flashggVBFTagMerger = cms.EDProducer("VBFTagMerger",src=cms.VInputTag("flashggVBFTag"))
+
+
+
+
 
 # Use JetID
 process.flashggVBFMVA.UseJetID      = cms.untracked.bool(True)
@@ -40,41 +68,109 @@ process.flashggVBFMVA.MVAMethod        = cms.untracked.string("BDTG")
 # combined MVA boundary set
 process.flashggVBFTag.Boundaries    = cms.untracked.vdouble(-2,0,2)
 
+process.systematicsTagSequences = cms.Sequence()
+
+# Do systematics on all MC (n.b. different from ordinary workspaces) but not on data
+if doSystematics:
+    systlabels = [""]
+    jetsystlabels = []
+    if customize.processId == "Data":
+        systprodlist = [getattr(process,"flashggJetSystematics%i"%i) for i in range(len(UnpackedJetCollectionVInputTag))]
+        for systprod in systprodlist:
+            systprod.SystMethods = cms.VPSet() # empty everything
+    else:
+        for direction in ["Up","Down"]:
+            jetsystlabels.append("JEC%s01sigma" % direction)
+            jetsystlabels.append("JER%s01sigma" % direction)
+            systlabels += jetsystlabels
+
+    for systlabel in systlabels:
+        if systlabel == "":
+            continue
+        newseq = cloneProcessingSnippet(process,process.flashggTagSequence,systlabel)
+        if systlabel in jetsystlabels:    
+            for i in range(len(jetSystematicsInputTags)):
+                massSearchReplaceAnyInputTag(newseq,jetSystematicsInputTags[i],cms.InputTag(jetSystematicsInputTags[i].moduleLabel,systlabel))
+        for name in newseq.moduleNames():
+            module = getattr(process,name)
+            if hasattr(module,"SystLabel"):
+                module.SystLabel = systlabel
+        process.systematicsTagSequences += newseq
+    process.flashggVBFTagMerger.src.append(cms.InputTag("flashggVBFTag" + systlabel))
+        
+        
 # set the VBF dumper
 process.vbfTagDumper = createTagDumper("VBFTag")
 process.vbfTagDumper.dumpTrees     = True
 process.vbfTagDumper.dumpHistos    = True
 process.vbfTagDumper.dumpWorkspace = False
+       
 
-# use the trigger-diphoton-preselection
-from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag
-massSearchReplaceAnyInputTag(process.flashggTagSequence,cms.InputTag("flashggDiPhotons"),cms.InputTag("flashggPreselectedDiPhotons"))
+if doSystematics:
+    process.vbfTagDumper.src = "flashggVBFTagMerger"
+else:
+    # use the trigger-diphoton-preselection
+    massSearchReplaceAnyInputTag(process.flashggTagSequence,cms.InputTag("flashggDiPhotons"),cms.InputTag("flashggPreselectedDiPhotons"))
 
 # get the variable list
 import flashgg.Taggers.VBFTagVariables as var
 all_variables = var.dijet_variables + var.dipho_variables + var.truth_variables
 
+cats = []
+if doSystematics:
+    for syst in jetsystlabels:
+        systcutstring = "hasSyst(\"%s\") "%syst
+        cats += [("VBFDiJet_%s"%syst,"leadingJet.pt>0&&%s"%systcutstring,0),
+                 ("excluded_%s"%syst,systcutstring,0)]
+else:
+    cats = [
+                    ("VBFDiJet","leadingJet.pt>0",0),
+                    ("excluded","1",0)
+                    ]
+
+cats += [
+    ("VBFDiJet","leadingJet.pt>0",0),
+    ("excluded","1",0)
+    ]
+
 cfgTools.addCategories(process.vbfTagDumper,
-                       [
-                           ("VBFDiJet","leadingJet.pt>0",0),
-                           ("excluded","1",0)
-                       ],
+                       cats,
                        variables  = all_variables,
                        histograms = []
 )
 
+print cats
+
+
 #process.vbfTagDumper.nameTemplate ="$PROCESS_$SQRTS_$LABEL_$SUBCAT_$CLASSNAME"
 process.vbfTagDumper.nameTemplate = "$PROCESS_$SQRTS_$CLASSNAME_$SUBCAT_$LABEL"
 
-from flashgg.MetaData.JobConfig import customize
 customize.setDefault("maxEvents" ,1000)    # max-number of events
 customize.setDefault("targetLumi",2.11e+3) # define integrated lumi
 customize(process)
 
-process.p1 = cms.Path(
-    process.flashggTagSequence*
-    #    process.flashggTagTester* # Uncommment if you what to test VBFtag
-    process.vbfTagDumper
-)
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring("HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_Mass95_v1") )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.hltRequirement = cms.Sequence()
+if customize.processId == "Data" and requireTriggerOnData:
+        process.hltRequirement += process.hltHighLevel
+
+if doSystematics:
+    process.p1 = cms.Path(
+        process.hltRequirement*
+        (process.flashggUnpackedJets*process.jetSystematicsSequence)*
+        (process.flashggTagSequence+process.systematicsTagSequences)*
+        process.flashggVBFTagMerger*
+        process.vbfTagDumper
+        )
+else:
+    process.p1 = cms.Path(
+        process.hltRequirement*
+        process.flashggTagSequence*
+        #    process.flashggTagTester* # Uncommment if you what to test VBFtag
+        process.vbfTagDumper
+        )
 
 print process.p1
