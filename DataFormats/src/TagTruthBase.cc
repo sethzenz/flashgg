1081d8accd76563f3b9a889497b8003354d6f6b2
Ed Scott <e.scott15@ic.ac.uk>
2017-08-09 18:02:25 +0100

    improve ggH weight accessing method

--- DataFormats/src/TagTruthBase.cc
+++ DataFormats/src/TagTruthBase.cc
@@ -1,5 +1,6 @@
 #include "flashgg/DataFormats/interface/TagTruthBase.h"
 #include "flashgg/DataFormats/interface/UncertaintyScheme.h"
+#include "FWCore/Utilities/interface/Exception.h"
 
 using namespace flashgg;
 
@@ -32,31 +33,41 @@ void TagTruthBase::copyBaseInfo( const TagTruthBase &b ) {
 // order: THU_ggH_Mu, THU_ggH_Res, THU_ggH_Mig01, THU_ggH_Mig12, THU_ggH_VBF2j, THU_ggH_VBF3j, THU_ggH_PT60, THU_ggH_PT120, THU_ggH_qmtop
 void TagTruthBase::setggHweights( int njets, float pTH, int stage1cat ) 
 {
-  std::vector<float> theWeightsUp  = qcd_ggF_uncertSF_2017( njets, pTH, stage1cat, 1. );
-  ggHweights_["THU_ggH_MuUp01sigma"]    = theWeightsUp[0];
-  ggHweights_["THU_ggH_ResUp01sigma"]   = theWeightsUp[1];
-  ggHweights_["THU_ggH_Mig01Up01sigma"] = theWeightsUp[2];
-  ggHweights_["THU_ggH_Mig12Up01sigma"] = theWeightsUp[3];
-  ggHweights_["THU_ggH_VBF2jUp01sigma"] = theWeightsUp[4];
-  ggHweights_["THU_ggH_VBF3jUp01sigma"] = theWeightsUp[5];
-  ggHweights_["THU_ggH_PT60Up01sigma"]  = theWeightsUp[6];
-  ggHweights_["THU_ggH_PT120Up01sigma"] = theWeightsUp[7];
-  ggHweights_["THU_ggH_qmtopUp01sigma"] = theWeightsUp[8];
+    std::vector<float> theWeightsUp  = qcd_ggF_uncertSF_2017( njets, pTH, stage1cat, 1. );
+    ggHweights_["THU_ggH_MuUp01sigma"]    = theWeightsUp[0];
+    ggHweights_["THU_ggH_ResUp01sigma"]   = theWeightsUp[1];
+    ggHweights_["THU_ggH_Mig01Up01sigma"] = theWeightsUp[2];
+    ggHweights_["THU_ggH_Mig12Up01sigma"] = theWeightsUp[3];
+    ggHweights_["THU_ggH_VBF2jUp01sigma"] = theWeightsUp[4];
+    ggHweights_["THU_ggH_VBF3jUp01sigma"] = theWeightsUp[5];
+    ggHweights_["THU_ggH_PT60Up01sigma"]  = theWeightsUp[6];
+    ggHweights_["THU_ggH_PT120Up01sigma"] = theWeightsUp[7];
+    ggHweights_["THU_ggH_qmtopUp01sigma"] = theWeightsUp[8];
 
-  std::vector<float> theWeightsDown = qcd_ggF_uncertSF_2017( njets, pTH, stage1cat, -1. );
-  ggHweights_["THU_ggH_MuDown01sigma"]    = theWeightsDown[0];
-  ggHweights_["THU_ggH_ResDown01sigma"]   = theWeightsDown[1];
-  ggHweights_["THU_ggH_Mig01Down01sigma"] = theWeightsDown[2];
-  ggHweights_["THU_ggH_Mig12Down01sigma"] = theWeightsDown[3];
-  ggHweights_["THU_ggH_VBF2jDown01sigma"] = theWeightsDown[4];
-  ggHweights_["THU_ggH_VBF3jDown01sigma"] = theWeightsDown[5];
-  ggHweights_["THU_ggH_PT60Down01sigma"]  = theWeightsDown[6];
-  ggHweights_["THU_ggH_PT120Down01sigma"] = theWeightsDown[7];
-  ggHweights_["THU_ggH_qmtopDown01sigma"] = theWeightsDown[8];
+    std::vector<float> theWeightsDown = qcd_ggF_uncertSF_2017( njets, pTH, stage1cat, -1. );
+    ggHweights_["THU_ggH_MuDown01sigma"]    = theWeightsDown[0];
+    ggHweights_["THU_ggH_ResDown01sigma"]   = theWeightsDown[1];
+    ggHweights_["THU_ggH_Mig01Down01sigma"] = theWeightsDown[2];
+    ggHweights_["THU_ggH_Mig12Down01sigma"] = theWeightsDown[3];
+    ggHweights_["THU_ggH_VBF2jDown01sigma"] = theWeightsDown[4];
+    ggHweights_["THU_ggH_VBF3jDown01sigma"] = theWeightsDown[5];
+    ggHweights_["THU_ggH_PT60Down01sigma"]  = theWeightsDown[6];
+    ggHweights_["THU_ggH_PT120Down01sigma"] = theWeightsDown[7];
+    ggHweights_["THU_ggH_qmtopDown01sigma"] = theWeightsDown[8];
 
-  //for( uint i=0; i<ggHweights_.size(); i++ ) {
-  //  std::cout << "Weight " << i << " = " << ggHweights_[i] << std::endl;
-  //}
+    // quick test
+    //std::cout << "ED DEBUG: " << HTXSggHweight("THU_ggH_MuUp01sigma") << std::endl;
+    //std::cout << "ED DEBUG: " << HTXSggHweight("nonsense") << std::endl;
+}
+
+float TagTruthBase::HTXSggHweight( std::string weightName ) const {
+    auto theIt = ggHweights_.find(weightName);
+    if( theIt != ggHweights_.end() ) {
+        return theIt->second;
+    }
+    else {
+        throw cms::Exception( "Missing Data" ) << "Could not find value (in the TagTruthBase) for the ggH weight called: " << weightName << "\n";;
+    }
 }
 
 
