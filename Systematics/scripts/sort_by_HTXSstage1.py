6d4b68dbd50c50e6dfb155709c9eb5e323504d0c
Edward Scott <e.scott15@ic.ac.uk>
2017-09-01 12:40:46 +0200

    small tweaks to stage 1 sorting

--- Systematics/scripts/sort_by_HTXSstage1.py
+++ Systematics/scripts/sort_by_HTXSstage1.py
@@ -10,19 +10,19 @@ stage1catname2num = {"UNKNOWN":0,
                      "GG2H_1J_PTH_0_60" : 104, "GG2H_1J_PTH_60_120" : 105, "GG2H_1J_PTH_120_200" : 106, "GG2H_1J_PTH_GT200" : 107,
                      "GG2H_GE2J_PTH_0_60" : 108, "GG2H_GE2J_PTH_60_120" : 109, "GG2H_GE2J_PTH_120_200" : 110, "GG2H_GE2J_PTH_GT200" : 111, 
                      # "VBF"
-                     "VBF_FWDH" : 200, "VBF_VBFTOPO_JET3VETO" : 201, "VBF_VBFTOPO_JET3" : 202, "VBF_VH2JET" : 203, "VBF_REST" : 204, "VBF_PTJET1_GT200" : 205
+                     "VBF_FWDH" : 200, "VBF_VBFTOPO_JET3VETO" : 201, "VBF_VBFTOPO_JET3" : 202, "VBF_VH2JET" : 203, "VBF_REST" : 204, "VBF_PTJET1_GT200" : 205,
                      # qq -> WH
-                     "QQ2HLNU_FWDH" = 300, "QQ2HLNU_PTV_0_150" = 301, "QQ2HLNU_PTV_150_250_0J" = 302, "QQ2HLNU_PTV_150_250_GE1J" = 303, "QQ2HLNU_PTV_GT250" = 304,
+                     "QQ2HLNU_FWDH" : 300, "QQ2HLNU_PTV_0_150" : 301, "QQ2HLNU_PTV_150_250_0J" : 302, "QQ2HLNU_PTV_150_250_GE1J" : 303, "QQ2HLNU_PTV_GT250" : 304,
                      # qq -> ZH
-                     "QQ2HLL_FWDH" = 400, "QQ2HLL_PTV_0_150" = 401, "QQ2HLL_PTV_150_250_0J" = 402, "QQ2HLL_PTV_150_250_GE1J" = 403, "QQ2HLL_PTV_GT250" = 404,
+                     "QQ2HLL_FWDH" : 400, "QQ2HLL_PTV_0_150" : 401, "QQ2HLL_PTV_150_250_0J" : 402, "QQ2HLL_PTV_150_250_GE1J" : 403, "QQ2HLL_PTV_GT250" : 404,
                      # gg -> ZH
-                     "GG2HLL_FWDH" = 500, "GG2HLL_PTV_0_150" = 501, "GG2HLL_PTV_GT150_0J" = 502, "GG2HLL_PTV_GT150_GE1J" = 503,
+                     "GG2HLL_FWDH" : 500, "GG2HLL_PTV_0_150" : 501, "GG2HLL_PTV_GT150_0J" : 502, "GG2HLL_PTV_GT150_GE1J" : 503,
                      # ttH
-                     "TTH_FWDH" = 600, "TTH" = 601,
+                     "TTH_FWDH" : 600, "TTH" : 601,
                      # bbH
-                     "BBH_FWDH" = 700, "BBH" = 701,
+                     "BBH_FWDH" : 700, "BBH" : 701,
                      # tH
-                     "TH_FWDH" = 800, "TH" = 801}
+                     "TH_FWDH" : 800, "TH" : 801}
 stage1catnum2name = {v: k for k, v in stage1catname2num.iteritems()}
 relevantstage1cats = [cat for cat in stage1catname2num.values()] #if cat > 0 and (cat <= 49 or cat == 60 or cat == 61)]
 #relevantstage1cats = [40] # for debugging
